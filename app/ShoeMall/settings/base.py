3d3fe8a5b1d2dcdf641ca801b26d7d9bba446c3f
Simowwn <125945116+Simowwn@users.noreply.github.com>
2025-02-22 14:55:50 +0800

    hey

--- app/ShoeMall/settings/base.py
+++ app/ShoeMall/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'user',
     'stall',
     'rest_framework',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'user.Users'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- app/ShoeMall/settings/dev.py
+++ app/ShoeMall/settings/dev.py
@@ -1,22 +1,59 @@
 from .base import *
 
-INSTALLED_APPS += []
-MIDDLEWARE += []
+INSTALLED_APPS += [
+    'corsheaders',
+    'rest_framework',
+    'rest_framework_simplejwt',
+]
 
-CORS_URLS_REGEX = r"^/api/.*$"
-CORS_ORIGIN_ALLOW_ALL = True
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # Must be at the top
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# CORS settings
+CORS_ALLOW_ALL_ORIGINS = True  # Temporarily set to True for testing
+CORS_ALLOW_CREDENTIALS = False  # Set to False for token authentication
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:8000",
-    "http://localhost:8001",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:8000",
-    "http://localhost:8001",
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+# Add CORS_EXPOSE_HEADERS
+CORS_EXPOSE_HEADERS = [
+    'authorization',
+]
+
+# Remove duplicate settings
+# CORS_URLS_REGEX = r"^/api/.*$"  # Remove this if you want all URLs to be accessible
 
 def main():
     """Run administrative tasks."""
