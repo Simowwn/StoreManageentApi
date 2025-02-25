cde7609dac5bf6a3fb6656e4ad6f303887e7e484
Simowwn <125945116+Simowwn@users.noreply.github.com>
2025-02-25 15:08:14 +0800

    dasdas

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -8,10 +8,17 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Users
-        fields = ("id", "email", "first_name", "last_name", "password")
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "is_staff",  # Staff status
+            
+        ]
         extra_kwargs = {"password": {"write_only": True}}
-
-
+        read_only_fields = ["user_permissions", "groups", "last_login", "date_joined", "is_active", "is_superuser"]
+        
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
